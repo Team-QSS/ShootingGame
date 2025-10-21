8879fbede3d81503016c37ddcb618de1d4c8bd44
ChanYoungOh <163986958+oty0365@users.noreply.github.com>
2025-10-21 15:47:26 +0900

    wip

--- Assets/Scripts/PlayerScripts/PlayerMove.cs
+++ Assets/Scripts/PlayerScripts/PlayerMove.cs
@@ -4,7 +4,7 @@ public class PlayerMove : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb2D;
     [SerializeField] private float speed;
-    //�̵� �Լ�
+    
     public void SetMove(Vector2 dir)
     {
         rb2D.linearVelocity = dir*speed;
